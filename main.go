b884921d3720a9fd0fd5e019a33764c486836b78
terrill <314156936@qq.com>
2018-08-14 19:25:14 +0800

    first and for all

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"bytes"
+	"time"
+)
+
+func main() {
+
+	path := os.Getenv("DEN")
+
+	fmt.Println(path)
+
+	pull := exec.Command("docker-compose", "-f", path, "pull")
+	down := exec.Command("docker-compose", "-f", path, "down")
+	up := exec.Command("docker-compose", "-f", path, "up")
+
+	ch := make(chan int)
+	pass := make(chan int)
+	end := make(chan int)
+	go func() {
+		down.Run()
+		ch <- 0
+		pass <- 0
+	}()
+	go func() {
+		<-pass
+		pull.Run()
+		ch <- 0
+		end <- 0
+	}()
+	go func() {
+		<-end
+		up.Start()
+		time.Sleep(8 * time.Second)
+		ch <- 0
+	}()
+
+	for i := 0; i < 100; i++ {
+		if i == 34 || i == 67 || i == 99{
+			<-ch
+		}
+		fmt.Print("\r")
+		for v := 0; v < 100; v++ {
+			if v < i {
+				fmt.Print("▇")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Print(" ", i+1, "%", getStr(i))
+		time.Sleep(50 * time.Millisecond)
+	}
+
+	fmt.Print("\r")
+	fmt.Print("den done")
+
+	cmd := exec.Command("docker-compose", "-f", path, "ps")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	cmd.Run()
+	fmt.Print(out.String())
+	close(ch)
+}
+
+func getStr(i int) string {
+	if i < 34 {
+		return " down"
+	}
+	if i > 66 {
+		return " up"
+	}
+	return " pull"
+}
